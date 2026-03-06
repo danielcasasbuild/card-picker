01d3faa183bf7d59cf7175235b470a9155ed119c
danielcasasbuild <danieliscasas@gmail.com>
2026-03-06 16:37:56 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const cacheName = "card-picker-v1";
+
+const base = self.location.pathname.replace(/\/[^/]*$/, "/");
+
+const assets = [
+  base,
+  base + "index.html",
+  base + "style.css",
+  base + "app.js",
+  base + "cards.json",
+  base + "manifest.json",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(assets)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== cacheName).map((k) => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => res || fetch(event.request))
+  );
+});
