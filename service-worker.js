beea4256f4a9f4602434afc22abe11d7518faa7a
danielcasasbuild <danieliscasas@gmail.com>
2026-03-06 17:08:40 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,36 @@
-const cacheName = "card-picker-v1";
+const cacheName = 'card-picker-v2'
 
-const base = self.location.pathname.replace(/\/[^/]*$/, "/");
+// Example: https://danielcasasbuild.github.io/card-picker/
+// Keep caching resilient to being served from a subpath.
+const base = self.location.pathname.replace(/\/[^/]*$/, '/')
 
 const assets = [
   base,
-  base + "index.html",
-  base + "style.css",
-  base + "app.js",
-  base + "cards.json",
-  base + "manifest.json",
-];
+  base + 'index.html',
+  base + 'style.css',
+  base + 'app.js',
+  base + 'cards.json',
+  base + 'manifest.json',
+]
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(assets)));
-});
-
-self.addEventListener("activate", (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== cacheName).map((k) => caches.delete(k)))
-    )
-  );
-});
+    caches.open(cacheName).then(cache => cache.addAll(assets))
+  )
+  self.skipWaiting()
+})
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== cacheName).map(k => caches.delete(k)))
+    )
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((res) => res || fetch(event.request))
-  );
-});
+    caches.match(event.request).then(res => res || fetch(event.request))
+  )
+})
